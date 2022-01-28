051bb161c16086d0053e1e79ec4ed541b13fd20f
Colin Williams <williamsca20@gmail.com>
2022-01-27 23:22:05 -0500

    🎻 Added 3D violin

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/common.scss'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 // Navbar hamburger menu
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,3 +27,73 @@ document.addEventListener('DOMContentLoaded', () => {
 		})
 	}
 })
+
+// 3D Stuff
+
+// Create Scene
+const scene = new THREE.Scene()
+
+// Add Camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	100
+)
+let cameraRadius = 5
+camera.position.z = cameraRadius
+
+// Add Renderer
+const canvas = document.querySelector('#canvas')
+const renderer = new THREE.WebGLRenderer({
+	powerPreference: 'low-power',
+	canvas: canvas,
+	alpha: true,
+})
+renderer.setPixelRatio(window.devicePixelRatio)
+
+// Set Window Size
+const setWindowSize = () => {
+	const height = 300
+	renderer.setSize(window.innerWidth, height)
+	camera.aspect = window.innerWidth / height
+	camera.updateProjectionMatrix()
+}
+setWindowSize()
+window.onresize = setWindowSize
+
+// Add Lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 2)
+scene.add(ambientLight)
+
+// Add Model Loader
+const modelLoader = new GLTFLoader()
+
+// Add Baritone Model
+// NOTE: Use glTF VS Code extension to convert .gltf to .glb
+let baritone
+modelLoader.load(
+	'../assets/3d/Violin.glb',
+	(gltf) => {
+		const scaleMultiplier = 2
+		gltf.scene.scale.set(scaleMultiplier, scaleMultiplier, scaleMultiplier)
+		baritone = gltf.scene
+		baritone.rotateX(-1)
+		baritone.rotateZ(4)
+		scene.add(baritone)
+	},
+	undefined,
+	(error) => {
+		console.error(error)
+	}
+)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+
+// Animation Loop
+function animate() {
+	requestAnimationFrame(animate)
+	controls.update()
+	renderer.render(scene, camera)
+}
+animate()
